2e2e6d5a5ed88eed8c0d618bf60d7fdd181096e8
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-04 16:49:23 +0800

    Added Navbar and Footer to App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <!-- Main layout components -->
-    <!-- <Navbar /> -->
+    <Navbar />
     <router-view />
-    <!-- <Footer /> -->
+    <Footer />
   </div>
 </template>
 
 <script setup>
-// import Navbar from "./components/Navbar.vue";
-// import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style>
