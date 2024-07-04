a853e532c7701746ec3a8247779cca291042a327
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-04 16:08:37 +0800

    Added components and views

---
+++ src/views/Projects.vue
@@ -0,0 +1,13 @@
+<!-- src/views/Projects.vue -->
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-white text-3xl font-bold mb-4">Projects</h1>
+    <p class="text-white text-lg text-center">Projects Page</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Add any scoped styles specific to this page if needed */
+</style>
