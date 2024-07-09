2b6d4cd8ef22597a95ade918125171a1b38a74db
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-09 23:06:03 +0800

    Updated Navbar styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,24 @@
 <!-- src/components/Navbar.vue -->
 <template>
-  <nav
-    class="fixed top-0 right-0 h-full text-white w-32 flex flex-col items-end justify-start mt-4 mr-4 p-4"
-  >
-    <router-link
-      v-for="(link, idx) in navLinks"
-      :key="idx"
-      :to="link.path"
-      class="my-4 text-lg"
-      :class="{ 'font-bold': isCurrentRoute(link.path) }"
+  <div class="nav-container relative">
+    <nav
+      class="fixed top-0 right-0 h-full text-white w-32 flex flex-col items-end justify-start mt-4 mr-4 p-4"
     >
-      {{ link.name }}
-    </router-link>
-  </nav>
+      <router-link
+        v-for="(link, idx) in navLinks"
+        :key="idx"
+        :to="link.path"
+        class="my-4 text-lg nav-link relative"
+        :class="{
+          'font-bold': isCurrentRoute(link.path),
+          'nav-link-active': isCurrentRoute(link.path),
+        }"
+      >
+        <span class="relative z-10">{{ link.name }}</span>
+        <div v-if="isCurrentRoute(link.path)" class="active-divider"></div>
+      </router-link>
+    </nav>
+  </div>
 </template>
 
 <script setup>
@@ -38,12 +44,36 @@ const isCurrentRoute = (routePath) => {
 <style scoped>
 /* Scoped styles for Navbar.vue */
 /* Adjust styles as per your design requirements */
+.nav-container {
+  position: relative;
+  display: flex;
+  justify-content: flex-end; /* Ensures content is aligned to the right */
+}
+
 .nav-link {
   text-decoration: none;
   color: white;
-  transition: background-color 0.3s ease;
+  transition: color 0.3s ease;
 }
+
 .nav-link:hover {
-  background-color: #4a5568;
+  color: #17ffc4;
+}
+
+.nav-link-active {
+  font-weight: bold;
+  color: #17ffc4;
+}
+
+.active-divider {
+  position: absolute;
+  top: 50%;
+  right: -10rem;
+  transform: translateY(-50%);
+  width: 20rem;
+  height: 2rem;
+  background-color: #ffd627;
+  z-index: 1; /* Ensure this is behind the text */
+  clip-path: polygon(0 0, 100% 0, 100% 100%, calc(100% - 18rem) 100%);
 }
 </style>
