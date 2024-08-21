272fe11f4bece8e874552e46d8f6023061b1aac6
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-08-21 18:02:49 -0400

    Added Navbar mobile screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,58 @@
 <!-- src/components/Navbar.vue -->
 <template>
   <div class="nav-container relative">
+    <!-- Mobile Menu Icon -->
+    <div
+      class="mobile-menu-icon md:hidden py-4 cursor-pointer"
+      @click="toggleMobileMenu"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        class="w-10 h-10 text-white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      </svg>
+    </div>
+
+    <!-- Mobile Slide-In Menu -->
+    <div
+      v-if="isMobileMenuOpen"
+      class="z-30 fixed top-0 right-0 h-full w-7/12 bg-darker_slate text-white px-8 shadow-lg transform transition-transform duration-300 ease-in-out md:hidden"
+    >
+      <span class="mb-12">&nbsp;</span>
+      <div class="flex justify-end">
+        <button class="text-white text-4xl" @click="toggleMobileMenu">
+          &times;
+        </button>
+      </div>
+      <nav class="flex flex-col items-end mt-8">
+        <router-link
+          v-for="(link, idx) in navLinks"
+          :key="idx"
+          :to="link.path"
+          class="my-4 text-xl nav-link relative"
+          @click="toggleMobileMenu"
+          :class="{
+            'font-bold': isCurrentRoute(link.path),
+            'nav-link-active': isCurrentRoute(link.path),
+          }"
+        >
+          <span class="relative z-10">{{ link.name }}</span>
+        </router-link>
+      </nav>
+    </div>
+
+    <!-- Desktop Navbar -->
     <nav
-      class="fixed top-0 right-0 h-full text-white w-32 flex flex-col items-end justify-start mt-4 mr-4 p-4"
+      class="hidden fixed top-0 right-0 h-full text-white w-32 md:flex flex-col items-end justify-start mt-4 mr-4 p-4"
     >
       <router-link
         v-for="(link, idx) in navLinks"
@@ -23,6 +73,7 @@
 
 <script setup>
 import { useRoute } from "vue-router";
+import { ref } from "vue";
 
 // Define your navigation links
 const navLinks = [
@@ -30,7 +81,6 @@ const navLinks = [
   { name: "About", path: "/about" },
   { name: "Projects", path: "/projects" },
   { name: "Experience", path: "/experience" },
-  // { name: "Resume", path: "/resume" },
   // { name: "Other", path: "/other" },
 ];
 
@@ -38,6 +88,12 @@ const navLinks = [
 const isCurrentRoute = (routePath) => {
   const currentRoute = useRoute();
   return currentRoute.path === routePath;
+};
+
+const isMobileMenuOpen = ref(false);
+
+const toggleMobileMenu = () => {
+  isMobileMenuOpen.value = !isMobileMenuOpen.value;
 };
 </script>
 
@@ -81,5 +137,18 @@ const isCurrentRoute = (routePath) => {
     100% 100%,
     calc(100% - calc(100% - 2.5rem)) 100%
   );
+}
+
+/* Mobile-specific styles */
+.mobile-menu-icon {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+}
+
+@media (min-width: 768px) {
+  .mobile-menu-icon {
+    display: none;
+  }
 }
 </style>
